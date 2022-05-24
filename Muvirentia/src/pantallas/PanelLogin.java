7daa20004ccf52820ee2a3e77ef63696383c852a
AritzGarzo <aritz.garzo19@gmail.com>
2022-05-24 14:24:23 +0200

    cambio de paneles. Comienzo

---
+++ Muvirentia/src/pantallas/PanelLogin.java
@@ -0,0 +1,20 @@
+package pantallas;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class PanelLogin extends JFrame {
+	JPanel panelLogin;
+	
+	public PanelLogin() {
+		panelLogin = new JPanel();
+	}
+	
+	
+	
+	
+	
+	public JPanel getPanel() {
+		return panelLogin;
+	}
+}
--- Muvirentia/src/principal/Principal.java
+++ Muvirentia/src/principal/Principal.java
@@ -1,25 +1,45 @@
 package principal;
 
+import java.awt.CardLayout;
 import java.awt.Container;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import pantallas.PanelLogin;
+
 public class Principal extends JFrame {
-	
+	JPanel panelVisual;
+
 	public Principal() {
 		super("Muvirentia");
-		
+		panelVisual = new JPanel(new CardLayout());
 		this.setSize(1000, 600);
 		this.setLocation(200, 100);
-		this.setContentPane(crearPanelVentana());
+		this.setContentPane(panelVisual);
+		cambiarPanel(crearPanelLogin());
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	
+
+	private JPanel crearPanelLogin() {
+		JPanel panel;
+		PanelLogin panelLogin = new PanelLogin();
+		panel = panelLogin.getPanel();
+		return panel;
+	}
+
+	private JPanel cambiarPanel(JPanel nuevoPanel) {
+		panelVisual.removeAll();
+		panelVisual.add(nuevoPanel);
+		panelVisual.repaint();
+		panelVisual.revalidate();
+		return panelVisual;
+	}
+
 	private Container crearPanelVentana() {
 		JPanel panel = new JPanel();
-		
+
 		return panel;
 	}
 
