a0731f628b2c3cae8f3ed3a239cdf60736e5f9ae
Txillu <qingyu.jiang@alumni.mondragon.edu>
2022-06-10 01:04:23 +0200

    backup

--- Muvirentia/src/baseDeDatos/Backup.java
+++ Muvirentia/src/baseDeDatos/Backup.java
@@ -7,7 +7,7 @@ public class Backup {
     public void backupSql() throws IOException {
         String ruta = "F:\\001UNI\\2.maila\\2.Semestre\\Base de datos\\pbl\\Muvirentia\\";
 
-        Process p = Runtime.getRuntime().exec("mysqldump -u root -p12345678aA@ -B -t muvirentia");
+        Process p = Runtime.getRuntime().exec("mysqldump -u root -p12345678aA@ -d -t muvirentia");
 
         InputStream is = p.getInputStream();
         FileOutputStream fos = new FileOutputStream(ruta+"backup_muvirentia.sql"); 
